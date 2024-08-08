802ef5565d4afcfa9f5ceab9cc72fd8f3861dd67
Szemesi Ádám <szemesiadam@teko.hu>
2024-08-08 13:30:33 +0200

    link

--- My_webpage/my_webpage.js
+++ My_webpage/my_webpage.js
@@ -6,10 +6,17 @@ function order(item){
     for(i = 0; i < oldalak.length; i++){
         if(item != oldalak[i]){
             document.getElementById(oldalak[i]).style.display = 'none' 
-            console.log(i)
         }
         else{
             document.getElementById(oldalak[i]).style.display = 'block'
         } 
+    }
+}
+document.getElementById("jslink").style.visibility = "hidden";
+function secret(){
+    let jelszo = document.getElementById("password").value;
+    if(jelszo == '18+'){
+        document.getElementById("jslink").style.visibility = "visible";
+        document.getElementById("kerdes").style.visibility = "hidden";
     }
 }
