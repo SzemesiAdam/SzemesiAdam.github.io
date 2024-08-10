df911177517671bb2cb2555cb301b4d75372155b
Szemesi Ádám <szemesiadam@teko.hu>
2024-08-10 22:29:04 +0200

    legördülő menu

--- My_webpage/my_webpage.js
+++ My_webpage/my_webpage.js
@@ -12,11 +12,18 @@ function order(item){
         } 
     }
 }
-document.getElementById("jslink").style.visibility = "hidden";
 function secret(){
     let jelszo = document.getElementById("password").value;
     if(jelszo == '18+'){
-        document.getElementById("jslink").style.visibility = "visible";
+        document.getElementById("jslink").style.display = "block";
         document.getElementById("kerdes").style.visibility = "hidden";
     }
+}
+
+document.querySelector(".menu-icon").onclick = function(){
+    document.querySelector("nav").classList.add("open");
+}
+
+document.querySelector(".close-icon").onclick = function(){
+    document.querySelector("nav").classList.remove("open");
 }
