fd2a12889c35bbd88e9f5c3025d2d5b6efca8cb7
Szemesi Ádám <szemesiadam@teko.hu>
2024-10-17 18:42:52 +0200

    g

--- Nyári szünet/js.js
+++ Nyári szünet/js.js
@@ -40,22 +40,19 @@ function link(){
     hiv.style.display = 'block'
 }
 
-let n = document.getElementById('nev');
-let a = document.getElementById('age');
-
 function tel(){
-    document.getElementById("data").innerHTML = n + " - " + a
-    document.getElementById("evszak").innerHTML = "A tél a kedvenc évszakod!"
+    document.getElementById("data").value = document.getElementById("nev").value + " - " + document.getElementById("age").value;
+    document.getElementById("evszak").value = "A tél a kedvenc évszakod!";
 }
 function tavasz(){
-    document.getElementById("data").innerHTML = nev + " - " + age
-    document.getElementById("evszak").innerHTML = "A tavasz a kedvenc évszakod!"
+    document.getElementById("data").value = document.getElementById("nev").value + " - " + document.getElementById("age").value;
+    document.getElementById("evszak").value = "A tavasz a kedvenc évszakod!";
 }
 function ny(){
-    document.getElementById("data").innerHTML = nev + " - " + age
-    document.getElementById("evszak").innerHTML = "A nyár a kedvenc évszakod!"
+    document.getElementById("data").value = document.getElementById("nev").value + " - " + document.getElementById("age").value;
+    document.getElementById("evszak").value = "A nyár a kedvenc évszakod!";
 }
 function osz(){
-    document.getElementById("data").innerHTML = nev + " - " + age
-    document.getElementById("evszak").innerHTML = "A ősz a kedvenc évszakod!"
+    document.getElementById("data").value = document.getElementById("nev").value + " - " + document.getElementById("age").value;
+    document.getElementById("evszak").value = "A ősz a kedvenc évszakod!";
 }
