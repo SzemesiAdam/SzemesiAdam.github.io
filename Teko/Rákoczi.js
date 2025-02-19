d3eea95d21027aa45ffc53a446118e3070197918
Szemesi Ádám <szemesiadam@teko.hu>
2025-02-19 11:21:03 +0100

    teko end

--- Teko/Rákoczi.js
+++ Teko/Rákoczi.js
@@ -2,7 +2,7 @@ let allas = false;
 let elozolap = "fooldal";
 let menukat = false;
 //weblap váltó
-const oldalak = ["fooldal", "reg"]
+const oldalak = ["fooldal", "reg", "bekuld"]
 function order(item){
     for(i = 0; i < oldalak.length; i++){
         if(item != oldalak[i]){
@@ -22,6 +22,10 @@ function bekuld(){
     let email = document.getElementById('email').value;
     let tel = document.getElementById('tel').value;
     let cs = document.querySelector('input[name="a"]:checked');
-
-    document.getElementById('valasz').innerHTML = "Beküldés sikeres " + nev;
+    // document.getElementById('valasz').innerHTML = "Beküldés sikeres " + nev;
+    order("bekuld")
+    document.getElementById('nevv').innerHTML = nev;
+    document.getElementById('emailv').innerHTML = email;
+    document.getElementById('telv').innerHTML = tel;
+    document.getElementById('csv').innerHTML = cs.value;
 }
