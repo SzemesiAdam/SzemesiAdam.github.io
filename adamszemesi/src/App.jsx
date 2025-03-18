c784dec5fba38c2545ca3438ad8ee137a88eced4
Szemesi Ádám <szemesiadam@teko.hu>
2025-03-18 15:06:43 +0100

    adamszemesi 1

--- adamszemesi/src/App.jsx
+++ adamszemesi/src/App.jsx
@@ -7,7 +7,11 @@ function App() {
 
   return (
     <>
-
+        <nav>
+          <button onClick={() => order('fooldal')}>Főoldal</button>
+          <button onClick={() => order('imerjmeg')}>Ismerj meg</button>
+          <button onClick={() => order('refmunkak')}>Referenciamunkák</button>
+        </nav>
     </>
   )
 }
